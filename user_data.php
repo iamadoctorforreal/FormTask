b00b5d3d9b5406f10f656cc7c20208291ac20cfb
iamadoctorforreal <52762924+iamadoctorforreal@users.noreply.github.com>
2022-05-28 18:28:10 +0100

    I still can't select and return the last input

--- user_data.php
+++ user_data.php
@@ -60,7 +60,10 @@ foreach (csv_content_parser($content4) as $fields) {
 }
 
 print_r($data);
-echo gettype($data);
+
+//The two lines below have refused to print the last input in my CSV file.
+So, I keep trying to solve it.
+
 print_r (end($data));
 print_r ($data[array_key_last($data)]);
 
