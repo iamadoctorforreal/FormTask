2cb1e45b4b5add55f279fae9042bfd71bd216191
iamadoctorforreal <52762924+iamadoctorforreal@users.noreply.github.com>
2022-05-28 17:45:55 +0100

    Form Task
    the Assignment
    
    echo end($data);
    
    refuses to work
    
    I tried returning the last array in the csv file but to no avail.
    it kept saying aray to string conversion as an error prompt that i couldn't fix, super frustrating

---
+++ user_data.php
@@ -0,0 +1,73 @@
+<?php
+$name = $_POST ["name"];
+$email = $_POST ["email"];
+$dob = $_POST ["date"];
+$gender = $_POST ["gender"];
+$country = $_POST ["country"];
+
+
+
+if(isset($_POST["submit"]))
+{
+
+$filename = "./userdata.csv";	
+
+$file_open = fopen($filename, "a");
+$no_rows = count(file($filename));
+if($no_rows > 1)
+{
+$no_rows = ($no_rows - 1) + 1;
+}
+$form_data = array(
+'sr_no' => $no_rows,
+'name' => $name,
+'email' => $email,
+'date' => $dob,
+'gender' => $gender,
+'country' => $country
+);
+
+fputcsv($file_open, $form_data, ",");
+fclose($file_open);
+
+
+
+$filename2 = "./userdata.csv";
+
+
+$file_open2 = fopen($filename2, "r");
+
+
+
+
+
+$content = fread($file_open2, filesize($filename2));
+
+$content2[] = fgetcsv($file_open2, filesize($filename2), ",");
+
+
+
+function csv_content_parser($content4) {
+	foreach (explode("\n", $content4)as $line){
+		yield str_getcsv($line);
+	}
+}
+
+$content4 = file_get_contents('./userdata.csv');
+$data =array();
+foreach (csv_content_parser($content4) as $fields) {
+	array_push($data, $fields);
+}
+
+print_r($data);
+echo gettype($data);
+print_r (end($data));
+print_r ($data[array_key_last($data)]);
+
+
+	fclose($file_open2);
+
+}
+
+
+?>
